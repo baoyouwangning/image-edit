4d60c63f89956f97d4d1bbac2137cac208f1b8d8
baoyouwangning <winvalx@qq.com>
2016-04-02 18:45:15 +0800

    use jest as test tool

---
+++ __tests__/component/image-canvas/test.getBase64.js
@@ -0,0 +1,15 @@
+// var chai = require('chai');
+// var assert = require('chai').assert;
+// var expect = chai.expect;
+// var getBase64 = require('../../../app/component/image-canvas/getBase64.jsx');
+
+// function sum(a, b) {
+//     return a + b;
+// }
+//
+// describe('sum', () => {
+//     it('adds 1 + 2 to equal 3', () => {
+//         const sum = require('../sum');
+//         expect(sum(1, 2)).toBe(3);
+//     });
+// });
