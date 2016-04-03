1dfa381e27a268f4ce6db9ffa57e4d09fd6b055c
baoyouwangning <winvalx@qq.com>
2016-04-03 11:47:51 +0800

    update test-case

--- __tests__/sum-test.js
+++ __tests__/sum-test.js
@@ -1,8 +1,10 @@
-jest.unmock('./sum'); // unmock to use the actual implementation of sum
+'use strict';
+
+jest.unmock('../app/sum'); // unmock to use the actual implementation of sum
 
 describe('sum', () => {
     it('adds 1 + 2 to equal 3', () => {
-        const sum = require('./sum');
+        const sum = require('../app/sum');
         expect(sum(1, 2)).toBe(3);
     });
 });
