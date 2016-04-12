ea71e211115fa69eed8d1d10aa59d0dcdf51935f
baoyouwangning <winvalx@qq.com>
2016-04-12 19:19:29 +0800

    fix repo

--- app/component/image-canvas/getBase64.jsx
+++ app/component/image-canvas/getBase64.jsx
@@ -6,7 +6,7 @@ class Hello extends React.Component {
     render() {
         return (
             <div className="component-wrapper">
-                Hi world!
+                Hi world!!
             </div>
         );
     }
