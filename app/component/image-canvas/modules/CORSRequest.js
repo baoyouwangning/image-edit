c94635654392fe86c7bb8b13aa6eeae9a62a80e8
baoyouwangning <winvalx@qq.com>
2016-04-16 16:46:05 +0800

    add CORSRequest module

---
+++ app/component/image-canvas/modules/CORSRequest.js
@@ -0,0 +1,20 @@
+var AsynObject = require('./AsynObject');
+var inheritPrototype = require('./inheritPrototype');
+
+//跨源请求包装器构造函数
+var CORSRequest = function (url,callback) {
+    AsynObject.call(this,XMLHttpRequest,callback);
+    this.initialize(url);
+};
+
+//使CORSRequest继承AsynObject
+inheritPrototype(CORSRequest,AsynObject);
+
+//跨源请求包装器初始化
+CORSRequest.prototype.initialize = function (url) {
+    this.obj.open("get",url,true);
+    this.obj.responseType = "blob";
+    this.obj.send(null);
+};
+
+module.exports = CORSRequest;
--- app/component/image-canvas/modules/image.js
+++ app/component/image-canvas/modules/image.js
@@ -1,20 +1,5 @@
 "use strict";
 
-
-//跨源请求包装器构造函数
-var CORSRequest = function (url,callback) {
-    AsynObject.call(this,XMLHttpRequest,callback);
-    this.initialize(url);
-};
-//使CORSRequest继承AsynObject
-inheritPrototype(CORSRequest,AsynObject);
-//跨源请求包装器初始化
-CORSRequest.prototype.initialize = function (url) {
-    this.obj.open("get",url,true);
-    this.obj.responseType = "blob";
-    this.obj.send(null);
-};
-
 //图片包装器构造函数 （注意现阶段url非blob URL)
 var SubImage = function (target,callback) {
     AsynObject.call(this,Image,callback);
