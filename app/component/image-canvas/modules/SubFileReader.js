c132d6820691b52a4b9a48cf686c57a74d4cd32f
baoyouwangning <winvalx@qq.com>
2016-04-16 16:31:07 +0800

    update SubFileReader

---
+++ app/component/image-canvas/modules/SubFileReader.js
@@ -0,0 +1,20 @@
+var AsynObject = require('./AsynObject');
+var inheritPrototype = require('./inheritPrototype');
+
+//读取本地图片文件包装器构造函数
+var SubFileReader = function (file,callback) {
+    AsynObject.call(this,FileReader,callback);
+    this.file = null;
+    this.initialize(file);
+};
+//使SubFileReader继承AsynObject
+inheritPrototype(SubFileReader,AsynObject);
+//读取本地图片文件初始化
+SubFileReader.prototype.initialize = function (file) {
+    this.file = file;
+};
+SubFileReader.prototype.readAsDataURL = function () {   //这里只用到了以数据URI的形式读取
+    this.obj.readAsDataURL(this.file);
+};
+
+module.exports = SubFileReader;
--- app/component/image-canvas/modules/image.js
+++ app/component/image-canvas/modules/image.js
@@ -1,23 +1,6 @@
 "use strict";
 
 
-
-//读取本地图片文件包装器构造函数
-var SubFileReader = function (file,callback) {
-    AsynObject.call(this,FileReader,callback);
-    this.file = null;
-    this.initialize(file);
-};
-//使SubFileReader继承AsynObject
-inheritPrototype(SubFileReader,AsynObject);
-//读取本地图片文件初始化
-SubFileReader.prototype.initialize = function (file) {
-    this.file = file;
-};
-SubFileReader.prototype.readAsDataURL = function () {   //这里只用到了以数据URI的形式读取
-    this.obj.readAsDataURL(this.file);
-};
-
 //跨源请求包装器构造函数
 var CORSRequest = function (url,callback) {
     AsynObject.call(this,XMLHttpRequest,callback);
